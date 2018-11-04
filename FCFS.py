fea59898cda5e4b6980ac6d327b4f7b46e08684e
RamySaied1 <ramy.m.saied@gmail.com>
2018-11-05 00:12:54 +0200

    FCFS completed

---
+++ FCFS.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import queue as Q
+
+
+
+def FCFS(input_file, context_switching):
+
+    input = open(input_file, 'r')
+
+    lines = input.readlines()
+    process_list = []
+    num_process = int(lines[0])
+
+    for i in range(0, num_process):
+        line = lines[i+1].split()
+        print(line)
+        process_list.append(Process(int(line[0]), float(
+            line[1]), float(line[2]), float(line[3]), 0, 0))
+
+    process_sorted_list = sorted(process_list, key=lambda Process: (
+        Process.arrival_time, Process.idd))
+
+    for i in range(0, num_process):
+        print(process_sorted_list[i].idd, process_sorted_list[i].arrival_time,
+              process_sorted_list[i].burst_time, process_sorted_list[i].priority, process_sorted_list[i].start_time, process_sorted_list[i].finish_time)
+
+    current_time = 0
+    q = Q.Queue()
+    result = []
+
+    while len(process_sorted_list) > 0:
+        if current_time <= process_sorted_list[0].arrival_time:
+            current_time = process_sorted_list[0].arrival_time
+
+        while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
+            q.put(process_sorted_list[0])
+            process_sorted_list.pop(0)
+
+        while not q.empty():
+            process = q.get()
+            process.start_time = current_time+context_switching
+            process.finish_time = process.start_time+process.burst_time
+            current_time = process.finish_time
+            result.append(process)
+            while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
+                q.put(process_sorted_list[0])
+                process_sorted_list.pop(0)
+
+    for i in range(0, len(result)):
+        print(str(result[i].idd)+" "+str(result[i].start_time) +
+              " "+str(result[i].finish_time)+" ")
+    return result
+
+
+class Process:
+    def __init__(self, idd, arrival_time, burst_time, priority, start_time, finish_time):
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.priority = priority
+        self.finish_time = finish_time
+        self.start_time = start_time
+        self.idd = idd
+
+
+result = FCFS("os_project/output.txt", 0)
+y = []
+start = []
+width = []
+for i in range(0, len(result)):
+    y.append(result[i].idd)
+    start.append(result[i].start_time)
+    width.append(result[i].finish_time-result[i].start_time)
+x1, x2, y1, y2 = plt.axis()
+plt.axis((x1, x2, 0, len(result)))
+plt.figure()
+plt.barh(height=y, width=width, left=start, y=0, align="edge")
+plt.show()
