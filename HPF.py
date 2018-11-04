d8a03723c923da085f696011a69e96c9d5e807fc
RamySaied1 <ramy.m.saied@gmail.com>
2018-11-04 23:33:10 +0200

    priority inversion

--- HPF.py
+++ HPF.py
@@ -32,7 +32,7 @@ def HPF(input_file,context_switching):
             current_time=process_sorted_list[0].arrival_time
 
         while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-            q.put((process_sorted_list[0].priority,process_sorted_list[0]))
+            q.put(((-process_sorted_list[0].priority, process_sorted_list[0].idd), process_sorted_list[0]))
             process_sorted_list.pop(0)
     
 
@@ -43,7 +43,7 @@ def HPF(input_file,context_switching):
             current_time=process.finish_time
             result.append(process)
             while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-                q.put((process_sorted_list[0].priority, process_sorted_list[0]))
+                q.put(((-process_sorted_list[0].priority,process_sorted_list[0].idd), process_sorted_list[0]))
                 process_sorted_list.pop(0)
 
 
