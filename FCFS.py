1f8cc48459fd112b0e9d3d9fb5253bff79017c3a
RamySaied1 <ramy.m.saied@gmail.com>
2018-11-24 15:58:43 +0200

    change logic in FCFS

--- FCFS.py
+++ FCFS.py
@@ -4,18 +4,19 @@ import queue as Q
 from Process import Process
 
 
-
 def FCFS(process_list, context_switching):
 
-    process_sorted_list = sorted(process_list, key=lambda Process: (
-        Process.arrival_time, Process.idd))
+    process_sorted_list = sorted(
+        process_list, key=lambda Process: Process.arrival_time)
 
     for i in range(0, len(process_list)):
         print(process_sorted_list[i].idd, process_sorted_list[i].arrival_time,
               process_sorted_list[i].burst_time, process_sorted_list[i].priority, process_sorted_list[i].start_time, process_sorted_list[i].finish_time)
 
     current_time = 0
-    q = Q.Queue()
+    # min heap to store current processes and return process with smallest arrival time
+    q = Q.PriorityQueue()
+
     result = []
 
     while len(process_sorted_list) > 0:
@@ -23,23 +24,26 @@ def FCFS(process_list, context_switching):
             current_time = process_sorted_list[0].arrival_time
 
         while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-            q.put(process_sorted_list[0])
+            q.put(((process_sorted_list[0].arrival_time,
+                    process_sorted_list[0].idd), process_sorted_list[0]))
             process_sorted_list.pop(0)
 
         while not q.empty():
-            process = q.get()
+            arrival, process = q.get()
+            print(" arrival ", arrival)
             process.start_time = current_time+context_switching
             process.finish_time = process.start_time+process.burst_time
             current_time = process.finish_time
             result.append(process)
             while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-                q.put(process_sorted_list[0])
+                q.put(
+                    ((process_sorted_list[0].arrival_time, process_sorted_list[0].idd), process_sorted_list[0]))
                 process_sorted_list.pop(0)
 
     for i in range(0, len(result)):
         print(str(result[i].idd)+" "+str(result[i].start_time) +
               " "+str(result[i].finish_time)+" ")
-    
+
     y = []
     start = []
     width = []
@@ -49,24 +53,22 @@ def FCFS(process_list, context_switching):
         y.append(result[i].idd)
         start.append(result[i].start_time)
         width.append(result[i].finish_time-result[i].start_time)
-        running_time[(result[i].start_time,result[i].finish_time)] = result[i].idd
+        running_time[(result[i].start_time, result[i].finish_time)
+                     ] = result[i].idd
 
     metric = np.zeros((len(result), 4)).astype(np.float)
 
     #filling
     for i in range(len(result)):
         metric[i, 0] = result[i].idd
-        metric[i, 2] = result[i].finish_time - result[i].arrival_time  # TAT = finish - arrival
+        metric[i, 2] = result[i].finish_time - \
+            result[i].arrival_time  # TAT = finish - arrival
         # Wating = TAT - burst_time
         metric[i, 1] = metric[i, 2]-result[i].burst_time
-        metric[i, 3] = 1.*metric[i, 2] /result[i].burst_time  # WTAT = TAT/burst_time
+        metric[i, 3] = 1.*metric[i, 2] / \
+            result[i].burst_time  # WTAT = TAT/burst_time
 
     return (metric, running_time)
-
-
-
-
-
 
 
 """result = FCFS("os_project/output.txt", 0)
--- HPF.py
+++ HPF.py
@@ -3,43 +3,45 @@ from Process import Process
 import numpy as np
 import queue as Q
 # assume lowest priority number is high priority
-def HPF(process_list,context_switching):
 
-    
-    process_sorted_list = sorted(process_list, key=lambda Process: (
-        Process.arrival_time, Process.priority));
 
-    for  i in range(0, len(process_list)):
-        print(process_sorted_list[i].idd,process_sorted_list[i].arrival_time,
+def HPF(process_list, context_switching):
+
+    # first sort only by arrival time
+    process_sorted_list = sorted(
+        process_list, key=lambda Process: Process.arrival_time)
+
+    for i in range(0, len(process_list)):
+        print(process_sorted_list[i].idd, process_sorted_list[i].arrival_time,
               process_sorted_list[i].burst_time, process_sorted_list[i].priority, process_sorted_list[i].start_time, process_sorted_list[i].finish_time)
-            
-    
-    current_time =0
-    q = Q.PriorityQueue()
-    result=[]
 
-    while len(process_sorted_list)>0:
-        if current_time <=process_sorted_list[0].arrival_time:
-            current_time=process_sorted_list[0].arrival_time
+    current_time = 0
+    q = Q.PriorityQueue()
+    result = []
+
+    while len(process_sorted_list) > 0:
+        if current_time <= process_sorted_list[0].arrival_time:
+            current_time = process_sorted_list[0].arrival_time
 
         while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-            q.put(((-process_sorted_list[0].priority, process_sorted_list[0].idd), process_sorted_list[0]))
+            q.put(
+                ((-process_sorted_list[0].priority, process_sorted_list[0].idd), process_sorted_list[0]))
             process_sorted_list.pop(0)
-    
 
         while not q.empty():
-            priotity,process= q.get()
-            process.start_time=current_time+context_switching
-            process.finish_time=process.start_time+process.burst_time
-            current_time=process.finish_time
+            priotity, process = q.get()
+            process.start_time = current_time+context_switching
+            process.finish_time = process.start_time+process.burst_time
+            current_time = process.finish_time
             result.append(process)
             while (len(process_sorted_list) > 0) and(process_sorted_list[0].arrival_time <= current_time):
-                q.put(((-process_sorted_list[0].priority,process_sorted_list[0].idd), process_sorted_list[0]))
+                q.put(
+                    ((-process_sorted_list[0].priority, process_sorted_list[0].idd), process_sorted_list[0]))
                 process_sorted_list.pop(0)
 
-
-    for i in range(0,len(result)):
-        print(str(result[i].idd)+" "+str(result[i].start_time)+" "+str(result[i].finish_time)+" ")
+    for i in range(0, len(result)):
+        print(str(result[i].idd)+" "+str(result[i].start_time) +
+              " "+str(result[i].finish_time)+" ")
 
     y = []
     start = []
@@ -50,20 +52,23 @@ def HPF(process_list,context_switching):
         y.append(result[i].idd)
         start.append(result[i].start_time)
         width.append(result[i].finish_time-result[i].start_time)
-        running_time[(result[i].start_time, result[i].finish_time) ] = result[i].idd
+        running_time[(result[i].start_time, result[i].finish_time)
+                     ] = result[i].idd
 
-    metric = np.zeros((len(result),4)).astype(np.float)
+    metric = np.zeros((len(result), 4)).astype(np.float)
 
-    #filling 
+    #filling
     for i in range(len(result)):
-        metric[i,0]=result[i].idd
-        metric[i, 2] = result[i].finish_time -result[i].arrival_time # TAT = finish - arrival
-        metric[i,1] = metric[i,2]-result[i].burst_time # Wating = TAT - burst_time
-        metric[i, 3] = 1.*metric[i, 2] / result[i].burst_time  # WTAT = TAT/burst_time
+        metric[i, 0] = result[i].idd
+        metric[i, 2] = result[i].finish_time - \
+            result[i].arrival_time  # TAT = finish - arrival
+        # Wating = TAT - burst_time
+        metric[i, 1] = metric[i, 2]-result[i].burst_time
+        metric[i, 3] = 1.*metric[i, 2] / \
+            result[i].burst_time  # WTAT = TAT/burst_time
 
-    print (metric)
-    return (metric,running_time)
-
+    print(metric)
+    return (metric, running_time)
 
 
 """result=HPF("./output.txt",0)
@@ -83,9 +88,3 @@ plt.figure()
 plt.barh(height=y, width=width , left=start,y=0,align="edge")
 plt.show()
 """
-
-
-
-
-
-
